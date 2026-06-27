b4f682018765257bbaddbd7395b9d35e10f58132
Andrey <cleverfoxcode@gmail.com>
2026-06-27 22:39:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,19 @@
 #include "services/logger/SerialLogger.h"
 #include "services/network/WiFiService.h"
 #include "services/heartbeat/HeartbeatService.h"
+#include "services/ota/OTAServer.h"
 
 static hub::services::SerialLogger     logger;
 static hub::services::WiFiService      wifiService;
+static hub::services::OTAServer        otaServer;
 static hub::services::HeartbeatService heartbeatService;
 static hub::core::IService*            services[] = {
     &logger,
     &wifiService,
+    &otaServer::IService*            services[] = {
+    &logger,
+    &wifiService,
+    &otaServer,
     &heartbeatService,
 };
 static hub::app::Application app(logger, services, sizeof(services) / sizeof(services[0]));
